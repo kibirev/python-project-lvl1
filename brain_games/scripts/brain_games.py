6c9a9d8a8eabf5b4af84018ff58f025fa1260c4c
kibirev <kibirev.v@gmail.com>
2022-06-10 13:29:10 +0300

    add cli.py and edited file brain_games.py and added module prompt

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+from cli import welcome_user
+
+
 def greet():
     print('Welcome to the Brain Games!')
 
 
 def main():
     greet()
+    print(welcome_user())
 
 
 if __name__ == '__main__':
