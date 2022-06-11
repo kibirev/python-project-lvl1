81d61df198ab07fbdbd4ed2e99f024911db22565
kibirev <kibirev.v@gmail.com>
2022-06-11 13:13:21 +0300

    edited brain_even.py

--- brain_games/scripts/__init__.py
+++ brain_games/scripts/__init__.py
@@ -1,4 +1,8 @@
-from .cli import welcome_user
-from .brain_even import parity_check
+from cli import welcome_user
+from brain_even import parity_check
+
+parity_check()
+welcome_user()
+
 
 __all__ = ["welcome_user", "parity_check"]
