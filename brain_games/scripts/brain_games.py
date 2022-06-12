008f6afdf7eb59b3917313d6e826ccd9d5bb9208
kibirev <kibirev.v@gmail.com>
2022-06-12 17:29:53 +0300

    added choice the game in brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from brain_even import parity_check
+from brain_calc import calculation
+import prompt
 
 
 def greet():
@@ -8,7 +10,14 @@ def greet():
 
 def main():
     greet()
-    parity_check()
+    print('Choose the game!\nPress \"1\": Brain Even\nPress \"2\": Brain Calc')
+    choice = prompt.string('Your choce: ')
+    if choice == '1':
+        parity_check()
+    elif choice == '2':
+        calculation()
+    else:
+        print('Incorrect input! Restart game, please!')
 
 
 if __name__ == '__main__':
