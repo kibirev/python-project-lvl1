27ef2a07bf6a02dc76f2c17ebef4aa2c131aef0e
kibirev <kibirev.v@gmail.com>
2022-06-19 18:49:25 +0300

    included generation of simple numbres

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -4,15 +4,17 @@ from random import randint
 
 
 def guessing_simple_number():
-    name = welcome_user('Answer "yes" if given number is prime. Otherwise answer "no".')
+    string = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    name = welcome_user(string)
     count_correct_answer = 0
     while True:
         number = randint(0, 100)
-        subsequence_simple_number = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
-                                     83, 89, 97]
+        subsequence = [2, 3, 5, 7]
+        for index in range(2, 32, 1):
+            subsequence.append(subsequence[index] + 6)
         player_answer = printing(number, '', '')
         is_simple_number = 'no'
-        for index in subsequence_simple_number:
+        for index in subsequence:
             if number == index:
                 is_simple_number = 'yes'
                 break
