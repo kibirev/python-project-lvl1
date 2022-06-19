3e3970767aa3b4a69f3ba354857510e539958dc7
kibirev <kibirev.v@gmail.com>
2022-06-19 17:09:01 +0300

    edited the logic of the brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -11,26 +11,26 @@ def parity_check():
         number = randint(0, 100)
         print(f'Question: {number}')
         player_answer = prompt.string('Your answer: ')
-        if player_answer == 'no':
+        if player_answer == 'yes':
             if number % 2 == 0:
                 correct_answer = counting_correct_answer(correct_answer)
                 if correct_answer == 3:
                     print(f'Congratulations, {name}!')
                     break
-                else:
-                    print(f"'{player_answer}' is wrong answer ;(. Correct answer was 'yes'.)\n"
-                          f"Let's try again, {name}!")
-                    break
+            else:
+                print(f"'{player_answer}' is wrong answer ;(. Correct answer was 'no'.)\n"
+                      f"Let's try again, {name}!")
+                break
         else:
             if number % 2 == 1:
                 correct_answer = counting_correct_answer(correct_answer)
                 if correct_answer == 3:
                     print(f'Congratulations, {name}!')
                     break
-                else:
-                    print(f"'{player_answer}' is wrong answer ;(. Correct answer was 'no'.)\n"
-                          f"Let's try again, {name}!")
-                    break
+            else:
+                print(f"'{player_answer}' is wrong answer ;(. Correct answer was 'yes'.)\n"
+                      f"Let's try again, {name}!")
+                break
 
 
 def main():
