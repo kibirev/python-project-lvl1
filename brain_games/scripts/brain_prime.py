de561e95423c38d9af7617ffacc29713b8f1d61c
kibirev <kibirev.v@gmail.com>
2022-06-19 21:36:24 +0300

    fixed is too complex

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
-from .cli import welcome_user, printing, counting, is_mistake
+from .cli import welcome_user, printing, checking_win
 from random import randint
+
+
+def generation_list():
+    temp = [2, 3, 5, 7]
+    for index in range(2, 32, 1):
+        temp.append(temp[index] + 6)
+    subsequence = []
+    for position in temp:
+        if position == 5 or position % 5 != 0:
+            if position == 7 or position % 7 != 0:
+                subsequence.append(position)
+    return subsequence
 
 
 def guessing_simple_number():
     string = 'Answer "yes" if given number is prime. Otherwise answer "no".'
     name = welcome_user(string)
-    count_correct_answer = 0
+    count_correct = 0
     while True:
         number = randint(0, 100)
-        temp = [2, 3, 5, 7]
-        for index in range(2, 32, 1):
-            temp.append(temp[index] + 6)
-        subsequence = []
-        for position in temp:
-            if position == 5 or position % 5 != 0:
-                if position == 7 or position % 7 != 0:
-                    subsequence.append(position)
+        subsequence = generation_list()
         player_answer = printing(number, '', '')
         is_simple_number = 'no'
         for index in subsequence:
@@ -25,13 +30,8 @@ def guessing_simple_number():
                 break
             else:
                 is_simple_number = 'no'
-        if player_answer == is_simple_number:
-            count_correct_answer = counting(count_correct_answer)
-            if count_correct_answer == 3:
-                print(f'Congratulations, {name}!')
-                break
-        else:
-            is_mistake(name, player_answer, is_simple_number)
+        count_correct = checking_win(name, player_answer, is_simple_number, count_correct)
+        if count_correct == 3 or count_correct == 0:
             break
 
 
