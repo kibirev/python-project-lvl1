d9ae9abdea00850b51434bf2038523018e46e03e
kibirev <kibirev.v@gmail.com>
2022-06-19 16:24:29 +0300

    edited the game's menu

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -12,10 +12,11 @@ def greet():
 
 
 def main():
+    prompt.string('May I have your name? ')
     greet()
     print(f'Choose the game!\nPress \"1\": Brain Even\nPress \"2\": Brain Calc\nPress \"3\": Brain GSD\n'
           f'Press \"4\": Brain Progression\nPress \"5\": Brain Prime\n')
-    choice = prompt.string('Your choce: ')
+    choice = prompt.string('Your choice: ')
     if choice == '1':
         parity_check()
     elif choice == '2':
