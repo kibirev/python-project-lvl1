94c60e2e26485b18f21010dcbd7df43a51239e6d
kibirev <kibirev.v@gmail.com>
2022-06-19 20:07:03 +0300

    fixed is too long string

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -7,19 +7,20 @@ def math_progression():
     name = welcome_user('What number is missing in the progression?')
     count_correct = 0
     while True:
-        start_number, step, after_before_secret_number = (randint(0, 100), randint(1, 10), randint(0, 9), )
+        start, step = (randint(0, 100), randint(1, 10), )
+        secret = randint(0, 9)
         string_left, string_right = ('', '',)
         correct = None
         for number in range(10):
-            if number < after_before_secret_number:
-                string_left = string_left + str(start_number) + ' '
-                start_number += step
-            elif number > after_before_secret_number:
-                string_right = string_right + ' ' + str(start_number)
-                start_number += step
+            if number < secret:
+                string_left = string_left + str(start) + ' '
+                start += step
+            elif number > secret:
+                string_right = string_right + ' ' + str(start)
+                start += step
             else:
-                correct = start_number
-                start_number += step
+                correct = start
+                start += step
         player = printing(string_left, string_right, '..')
         count_correct = checking_win(name, player, correct, count_correct)
         if count_correct == 3 or count_correct == 0:
