5a75dab8fe98e87e0d204b185b7ffefda549ad1e
kibirev <kibirev.v@gmail.com>
2022-06-20 20:07:19 +0300

    fixed is too long string

--- brain_games/__init__.py
+++ brain_games/__init__.py
@@ -1,5 +1,5 @@
-from scripts.cli import welcome_user, checking_win, printing
-from scripts.brain_even import parity_check
+from .scripts.cli import welcome_user, checking_win, printing
+from .scripts.brain_even import parity_check
 
 
 __all__ = ["welcome_user", "parity_check", "checking_win", "printing"]
